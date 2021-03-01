e06776493e036e828120f9f6e4d99b937f776e9e
Alberto González Pons <albgonpon@gmail.com>
2021-03-01 18:00:28 +0100

    deletion

--- app/@esri/calcite-components/dist/collection/utils/CalciteIcon.js
+++
@@ -1,5 +0,0 @@
-import { h } from "@stencil/core";
-export const CalciteIcon = ({ path, size, svgAttributes, title }) => (h("svg", Object.assign({ xmlns: "http://www.w3.org/2000/svg", height: size, width: size, fill: "currentColor", viewBox: `0 0 ${size} ${size}` }, svgAttributes),
-    title ? h("title", null, title) : null,
-    h("path", { d: path })));
-export default CalciteIcon;
--- app/@esri/calcite-components/dist/types/utils/filter.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const filter: (data: object[], value: string) => any[];
